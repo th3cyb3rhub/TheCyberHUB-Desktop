9874570b240c85acb2a74bfbbbdbcc1b9d9fac94
Kabir <44284877+kabir0x23@users.noreply.github.com>
2024-11-01 17:28:21 +0530

    feat: icons

--- src/main.js
+++ src/main.js
@@ -2,31 +2,30 @@ const { app, BrowserWindow } = require('electron');
 const path = require('path');
 
 function createWindow() {
+    const iconPath = path.join(__dirname, 'th3cyb3rhub-fav.icns');
+
     const win = new BrowserWindow({
         width: 800,
         height: 600,
+        icon: iconPath,
         webPreferences: {
-            preload: path.join(__dirname, 'preload.js'), // Path to preload.js
-            contextIsolation: true, // Recommended for security
-            enableRemoteModule: false // Disable remote module
+            preload: path.join(__dirname, 'preload.js'),
+            contextIsolation: true,
+            enableRemoteModule: false
         },
     });
 
-    // Load the built React app from the 'build' folder (adjust the path as necessary)
     win.loadFile(path.join(__dirname, '../dist/index.html'));
 }
 
-// Quit the application when all windows are closed
 app.on('window-all-closed', () => {
     app.quit();
 });
 
-// Create a window when the application is activated
 app.on('activate', () => {
     if (BrowserWindow.getAllWindows().length === 0) {
         createWindow();
     }
 });
 
-// Start the application and create the window
 app.whenReady().then(createWindow);
